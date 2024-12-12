786ffcc507df3a708d490c1b63b3fdfb8f0643d3
Ioioiog <158222833+Ioioiog@users.noreply.github.com>
2024-12-12 22:35:00 +0200

    comm

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: 'dreamy-property-finder-main-2',
+  base: '/dreamy-property-finder-main-2',
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
